a9f9dc329189faac4b453926767a24eb0f15d114
Antonio <antonio.perez@valispace.com>
2022-06-13 19:34:17 +0200

    Non working functionality Added login. Started refactoring.

--- valispace/src/config.jsx
+++ valispace/src/config.jsx
@@ -1,37 +1,57 @@
-import ForgeUI, { render, useState, Button, ProjectPage, Fragment, Text } from '@forge/ui';
-import api, { route } from '@forge/api';
-import { getVerificationActivities, requestValispace, valiReqIdentifier } from './valispace';
+import ForgeUI,
+{ render,
+    useState,
+    Button,
+    ProjectPage,
+    Fragment,
+    Text,
+    Form,
+    Select,
+    TextField } from '@forge/ui';
+import api, { route, storage } from '@forge/api';
+import { getVerificationActivities, requestValispace, updateOrCreateCards, valiReqIdentifier } from './valispace';
 
 
-const LinkedReqsText = ( {number} ) => {
-    if (number > 0) {
-        return <Text>{number} requirement{number > 1 ? 's' :''} synced</Text>;
+const LoginForm = ({ }) => {
+    const getSavedValues = async (key) => {
+        return await storage.getSecret(key)
     }
-    return false;
-}
+    const [valispace_url, setValispace_url] = useState(getSavedValues("valispace_url") || '');
+    const [valispace_username, setValispace_username] =  useState(getSavedValues("valispace_username") || '');
+    const [valispace_token, setValispace_token] =  useState(getSavedValues("valispace_token") || '');
+    const [valispace_password, setValispace_password] =  useState(getSavedValues("valispace_password") || '');
+    const [valispace_project, setValispace_project] = useState(getSavedValues("valispace_project") || '');
+    const onSubmit = async (formData) => {
+        for (const [key, value] of Object.entries(formData)) {
+            storage.setSecret(key, value);
+        }
+        // console.log(getSavedValues('valispace_token'))
+        setValispace_url(formData["valispace_url"]);
+        setValispace_username(formData["valispace_username"]);
+        setValispace_password(formData["valispace_password"]);
+        setValispace_project(formData["valispace_project"]);
 
-/*
-const buildCardFromReq = ( data ) => {
-    return {
-        "fields": {
-            "summary": data.identifier,
-            "project": {
-                "key": "VTS2"
-            },
-            "issuetype": {
-                "id": "10005"
-            },
-        },
-        "properties": [
-            {
-                "key" : "valiReq" ,
-                "value": `${data.id}`
-            }
-        ]
-    };
+    }
+    const goBack = () => {};
+    const cancel = () => {};
+    // The array of additional buttons.
+    // These buttons align to the right of the submit button.
+    const actionButtons = [
+        <Button text="Go back" onClick={goBack} />,
+        <Button text="Cancel" onClick={cancel} />,
+    ];
 
+
+    return (
+        <Form onSubmit={onSubmit} actionButtons={actionButtons}>
+            <TextField name="valispace_url" label="Valispace url" isRequired defaultValue={valispace_url}/>
+            <TextField name="valispace_username" label="Username" isRequired defaultValue={valispace_username}/>
+            {/* <TextField name="valispace_token" label="Token" isRequired defaultValue={valispace_token}/> */}
+            <TextField name="valispace_password" label="Password" type="password" isRequired defaultValue={valispace_password}/>
+            <TextField name="valispace_project" label="Project Id" type="number" isRequired defaultValue={valispace_project}/>
+        </Form>
+    )
 }
-*/
 
 const getIssueValiReq = async ( issue_key ) => {
     return api.asApp().requestJira(route`/rest/api/3/issue/${issue_key}/properties/valiReq`, {
@@ -43,152 +63,16 @@ const getIssueValiReq = async ( issue_key ) => {
     });
 }
 
-const bulkCreateCards = async ( data ) => {
-    return api.asApp().requestJira(route`/rest/api/3/issue/bulk`, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: data
-    });
-}
-
-const getValiReqMapping = async (project_name) => {
-    const req_mapping = {};
-
-    let result = await api.asApp().requestJira(route`/rest/api/3/search?jql=project=${project_name} and issue.property[valiReq] is not empty &startAt=0&maxResults=8000&fields=issue`, {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-    });
-
-    const data = await result.json();
-
-    for (let issue of data.issues) {
-        // console.log(`Issue: ${issue.key}`);
-
-        result = await getIssueValiReq(issue.key);
-        const props = await result.json();
-        const req_identifier = valiReqIdentifier(props);
-
-        // console.log(req_identifier);
-
-        if (req_identifier != null) {
-            req_mapping[req_identifier] = issue.key;
-        }
-    }
-
-    return req_mapping;
-}
-
-/*
-const createCardsFromRequirements = async ( reqs ) => {
-    let newCards = [];
-    reqs.forEach(( req ) => {
-        newCards.push(buildCardFromReq(req));
-    });
-    const bulkUpdateFormat = {
-        "issueUpdates": newCards
-    }
-    console.log(bulkUpdateFormat);
-    console.log(await (await bulkCreateCards(JSON.stringify(bulkUpdateFormat))).text());
-}
-*/
 
 const App = () => {
-    const [linkedReq, setLinkedReq] = useState(0);
-
-    /*
-    const initialSyncClick = async () => {
-        let new_reqs = await getVerificationActivities();
-        new_reqs = Object.values(new_reqs);
-
-        console.log("new_reqs", new_reqs);
-
-        if (new_reqs.length > 0) {
-            const bulk_update_format = {
-                "issueUpdates": new_reqs,
-            };
-
-            console.log(JSON.stringify(bulk_update_format));
-
-            const result = await bulkCreateCards(JSON.stringify(bulk_update_format));
-            console.log(await result.text());
-        }
-        else {
-            console.log("No new requirements.");
-        }
-    }
-    */
-
-    const updateClick = async () => {
-        const req_mapping = await getValiReqMapping('VTS');
-        const reqs = await getVerificationActivities();
-
-        const new_reqs = [];
-
-        // identifier is a mix of different ids
-        for (let identifier in reqs) {
-            if (identifier in req_mapping) {
-                // apparently there's no bulk update in jira
-                const data = reqs[identifier];
-
-                console.log(JSON.stringify(data, null, '\t'));
-
-                console.log(`identifier = ${identifier}`);
-                console.log(`reqs[identifier] = ${reqs[identifier]}`);
-                console.log(`req_mapping[identifier] = ${req_mapping[identifier]}`);
-
-                /*let result = await api.asApp().requestJira(route`/rest/api/3/issue/${req_mapping[identifier]}`, {
-                    method: 'GET',
-                    headers: {
-                      'Accept': 'application/json',
-                      'Content-Type': 'application/json'
-                    }
-                });
-
-                console.log(await result.text());*/
-
-                console.log(`/rest/api/3/issue/${req_mapping[identifier]}`);
-
-                let result = await api.asApp().requestJira(route`/rest/api/3/issue/${req_mapping[identifier]}`, {
-                    method: 'PUT',
-                    headers: {
-                      'Accept': 'application/json',
-                      'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(data)
-                });
-
-                console.log(await result.text());
-            }
-            else {
-                new_reqs.push(reqs[identifier]);
-            }
-        }
-
-        if (new_reqs.length > 0) {
-            const bulk_update_format = {
-                "issueUpdates": new_reqs,
-            };
-
-            // console.log(JSON.stringify(bulk_update_format));
-
-            const result = await bulkCreateCards(JSON.stringify(bulk_update_format));
-            console.log(await result.text());
-        }
-        else {
-            console.log("No new requirements.");
-        }
-    }
 
 
+    const updateClick = async () => { updateOrCreateCards()}
+
+    // TODO: Disable button if not checkValispaceConnexion
     return (
         <Fragment>
-            <LinkedReqsText number={linkedReq} />
+            <LoginForm/>
             <Button onClick={updateClick} text="Update from Valispace"></Button>
         </Fragment>
     )
@@ -202,18 +86,14 @@ export const run = render(
 
 export async function issueUpdate(event, context) {
 	console.log('issueUpdate');
-    // console.log('event:');
-    // console.log(JSON.stringify(event, null, '\t'));
-    // console.log('context:');
-    // console.log(JSON.stringify(context, null, '\t'));
 
     const changes = event.changelog.items;
 
+
     for (let change of changes) {
-        // console.log(change);
+
 
         if (change.fieldtype == 'jira') {
-            'resolution'
             if (change.field == 'status' || change.fieldId == 'status') {
                 // console.log("New status...");
 
--- valispace/src/valispace.js
+++ valispace/src/valispace.js
@@ -1,14 +1,78 @@
-import { VALISPACE_URL, VALISPACE_TOKEN, VALISPACE_USERNAME, VALISPACE_PROJECT } from './constants';
-import { fetch } from '@forge/api';
+import api, { fetch, storage } from '@forge/api';
 
+
+
+
+
+const valispaceAskToken = async (deployment_url, username, passwd) => {
+    //  var dialog = DocumentApp.getUi()
+
+    var tokenUrl = deployment_url + 'o/token/'
+    var data = {
+      grant_type: 'password',
+      client_id: 'ValispaceREST',
+      username: username,
+      password: passwd
+    };
+    const url = new URL(tokenUrl);
+
+    for (let i in data) {
+        url.searchParams.append(i, data[i]);
+    }
+
+    var options = {
+        method: 'POST'
+    };
+    var response = await fetch(url.toString(), options);
+    var responseData = await response.json();
+    if (!responseData || !responseData.access_token) {
+        throw new Error(`Login Error`)
+    }
+    var accessToken = responseData.access_token;
+    return accessToken
+}
+
+const valispaceGetToken = async (deployment_url) => {
+    const connected = await checkValispaceConnexion();
+    console.log(connected)
+    let accessToken = await storage.getSecret("access_token")
+    if (!connected || accessToken === undefined) {
+        const VALISPACE_USERNAME = await storage.getSecret("valispace_username");
+        const VALISPACE_PASSWORD = await storage.getSecret("valispace_password");
+        accessToken = await valispaceAskToken(
+            deployment_url,
+            VALISPACE_USERNAME,
+            VALISPACE_PASSWORD
+        );
+
+      storage.setSecret("access_token", accessToken);
+    }
+    return accessToken;
+}
+
+ export const checkValispaceConnexion = async () => {
+
+    const response = await requestValispace('project');
+    console.log(response.text())
+    return response.ok;
+
+}
 
 export const requestValispace = async ( path, method = 'GET', url_params = {}, data = null ) => {
+    const VALISPACE_URL = await storage.getSecret('valispace_url');
+    const VALISPACE_PROJECT = await storage.getSecret('valispace_project');
+
+
     const url = new URL(VALISPACE_URL + path);
     url.searchParams.append('project', VALISPACE_PROJECT);
 
     for (let i in url_params) {
         url.searchParams.append(i, url_params[i]);
     }
+
+
+    // const VALISPACE_TOKEN = await storage.getSecret("access_token")
+    const VALISPACE_TOKEN = await valispaceGetToken(VALISPACE_URL)
 
     const fetch_options = {
         method: method,
@@ -20,9 +84,13 @@ export const requestValispace = async ( path, method = 'GET', url_params = {}, d
     if (data !== null) {
         fetch_options['body'] = JSON.stringify(data);
     }
+    console.log(fetch_options);
+
+
+
 
     return fetch(
-        url.toString(), fetch_options);
+        url.toString(), fetch_options)
 }
 
 const downloadRequirements = async () => {
@@ -74,12 +142,65 @@ export const valiReqIdentifier = (props) => {
     }
 }
 
+export const updateOrCreateCards = async () => {
+    req_mapping = await getValiReqMapping('VTS');
+    const reqs = await getVerificationActivities();
+
+    const new_reqs = [];
+
+    // identifier is a mix of different ids
+    for (let identifier in reqs) {
+        if (identifier in req_mapping) {
+            // apparently there's no bulk update in jira
+            const data = reqs[identifier];
+
+            console.log(JSON.stringify(data, null, '\t'));
+
+            console.log(`identifier = ${identifier}`);
+            console.log(`reqs[identifier] = ${reqs[identifier]}`);
+            console.log(`req_mapping[identifier] = ${req_mapping[identifier]}`);
+
+
+
+            console.log(`/rest/api/3/issue/${req_mapping[identifier]}`);
+
+            let result = await api.asApp().requestJira(route`/rest/api/3/issue/${req_mapping[identifier]}`, {
+                method: 'PUT',
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            console.log(await result.text());
+        }
+        else {
+            new_reqs.push(reqs[identifier]);
+        }
+}
+
+if (new_reqs.length > 0) {
+    const bulk_update_format = {
+        "issueUpdates": new_reqs,
+    };
+
+    // console.log(JSON.stringify(bulk_update_format));
+
+    const result = await bulkCreateCards(JSON.stringify(bulk_update_format));
+    console.log(await result.text());
+}
+else {
+    console.log("No new requirements.");
+}
+}
 export const getVerificationActivities = async () => {
     const new_requirements = {};
 
     // get final state id
     let result = await requestValispace(`rest/requirements/states/`, 'GET');
     const states = await result.json();
+    console.log(states);
     let final_id = -1;
     for (let state of states) {
         if (state['name'] == 'Final') {
