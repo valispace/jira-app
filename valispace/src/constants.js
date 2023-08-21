e07c5f093a00eb220236bd57934ea81d9584ad73
Cauê Napier <cauenapier@users.noreply.github.com>
2023-08-21 15:33:16 +0100

    Delete constants.js
    Deleting file with user credentials - User doesn't exist anymore, it is safe to share

--- valispace/src/constants.js
+++
@@ -1,4 +0,0 @@
-export const VALISPACE_URL = 'https://integrations.valispace.com/'
-export const VALISPACE_USERNAME = 'tiago'
-export const VALISPACE_TOKEN = 'gtYoLX8RkdSvZS5qXwL7oBVnNOyo2c'
-export const VALISPACE_PROJECT = 41
